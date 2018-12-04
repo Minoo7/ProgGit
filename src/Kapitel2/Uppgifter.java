771bcae38f67c04a688be64f076a41bb238373cc
Minoo7 <accounts@minoo.pro>
2018-12-04 10:19:40 +0100

    2.1

---
+++ src/Kapitel2/Uppgifter.java
@@ -0,0 +1,18 @@
+public class Uppgifter {
+
+    public static void main(String[] args) {
+        uppgift2_1();
+        System.out.println();
+    }
+
+    static void uppgift2_1() {
+        double talA = 2.1;
+        double talB = 3.1;
+        double summan = talA + talB;
+        System.out.println("Tal A är " + talA);
+        System.out.println("Tal B är " + talB);
+        System.out.println("Summan är " + summan);
+    }
+
+
+}
