e1101d883e6a6fe6c0e817cffa65b90a8df86cac
Minoo7 <accounts@minoo.pro>
2018-12-04 09:19:04 +0100

    Initial commit

---
+++ src/Kapitel1/MyProgram.java
@@ -0,0 +1,14 @@
+public class MyProgram {
+
+    public static void main(String[] args) {
+        // Variabler skapas med typ namn = värde;
+        int myint = 7;
+        String mystring = "Hejsan ";
+        System.out.println(mystring + myint);
+
+        System.out.println("Hello world!");
+
+    }
+
+
+}
