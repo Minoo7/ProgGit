b52b3936a4de33b6e3b5ee37aa813b7e67fb5489
Minoo7 <accounts@minoo.pro>
2018-12-11 08:34:20 +0100

    2.2

--- src/Kapitel2/Uppgifter.java
+++ src/Kapitel2/Uppgifter.java
@@ -1,7 +1,11 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Uppgifter {
 
     public static void main(String[] args) {
-        uppgift2_1();
+        //uppgift2_1();
+        test();
         System.out.println();
     }
 
@@ -12,6 +16,37 @@ public class Uppgifter {
         System.out.println("Tal A är " + talA);
         System.out.println("Tal B är " + talB);
         System.out.println("Summan är " + summan);
+    }
+    static void test() {
+        Scanner input = new Scanner( System.in );
+
+        System.out.println("Skriv namn:");
+        String namn = input.nextLine();
+
+        System.out.println("Skriv ålder: ");
+        int ålder = input.nextInt();
+
+        System.out.println("Skriv längd: ");
+        int längd = input.nextInt();
+        int tlängd;
+        if (längd >= 100 ) {
+            tlängd = längd/100;
+        }
+        else {
+            tlängd = längd;
+        }
+
+
+        System.out.println(namn + " är " + ålder + " år gammal och " + tlängd + " m lång.");
+        System.out.println("Stämmer det?");
+        String rätt;
+        String svar = input.nextLine();
+        if (svar.equals("ja")) {
+            rätt = "Vad bra!";
+        }
+        else {
+            rätt = "Ajdå!";
+        }
     }
 
 
