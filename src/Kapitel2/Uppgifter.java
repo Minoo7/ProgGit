b4c9a031dd5ae472e471e7d92df63ccae4de538e
Minoo7 <accounts@minoo.pro>
2018-12-12 11:28:31 +0100

    2018_12_12

--- src/Kapitel2/Uppgifter.java
+++ src/Kapitel2/Uppgifter.java
@@ -1,11 +1,13 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 import java.util.*;
 
 public class Uppgifter {
 
     public static void main(String[] args) {
-        looptest();
-        //byteValueTest();
+        //repetitionsfrågor();
+        //looptest();
+        byteValueTest();
         //System.out.println();
         //uppgift2_1();
         //System.out.println();
@@ -14,11 +16,82 @@ public class Uppgifter {
         //v50Torsdag();
     }
 
-    static void looptest() {
+    static void repetitionsfrågor() {
+        Scanner input = new Scanner( System.in );
+        int numberOfCorrectAnswers = 0;
+
+        System.out.println("Skriv de commands som passar varje uppgift");
+        System.out.println("Använd tal och tecken som variabel namn beroende på uppgiften");
+        System.out.println();
+        System.out.println("a)  Reservera 4 byte för et tnytt heltal.");
+        String a = input.nextLine();
+        if (a.equals("int tal;")) {
+            System.out.println("Rätt!");
+            numberOfCorrectAnswers = numberOfCorrectAnswers + 1;
+        }
+        else {
+            System.out.println("Fel!");
+        }
+        System.out.println();
+
+        System.out.println("b)  Reservera 2 byte för ett nytt heltal och ge det värdet 5");
+        String b = input.nextLine();
+        if (b.equals("short tal = 5;")) {
+            System.out.println("Rätt!");
+            numberOfCorrectAnswers = numberOfCorrectAnswers + 1;
+        }
+        else {
+            System.out.println("Fel!");
+        }
+        System.out.println();
+
+        System.out.println("c)  Reservera 4 byte för ett nytt flyttal och ge värdet 3,3.");
+        String c = input.nextLine();
+        if (c.equals("float tal = 3.3f;")) {
+            System.out.println("Rätt!");
+            numberOfCorrectAnswers = numberOfCorrectAnswers + 1;
+        }
+        else {
+            System.out.println("Fel!");
+        }
+        System.out.println();
+
+        System.out.println("d)  Reservera 8 byte för ett nytt flyttal och ge värdet 9,90.");
+        String d = input.nextLine();
+        if (d.equals("double tal = 9.90;")) {
+            System.out.println("Rätt!");
+            numberOfCorrectAnswers = numberOfCorrectAnswers + 1;
+        }
+        else {
+            System.out.println("Fel!");
+        }
+        System.out.println();
+
+        System.out.println("e)  Reservera 2 byte för ett nytt tecken och ge det värdet T.");
+        String e = input.nextLine();
+        if (e.equals("char tecken = T;")) {
+            System.out.println("Rätt!");
+            numberOfCorrectAnswers = numberOfCorrectAnswers + 1;
+        }
+        else {
+            System.out.println("Fel!");
+        }
+        System.out.println();
+
+        System.out.println();
+        System.out.println("Bra jobbat!");
+        System.out.println("Du fick " + numberOfCorrectAnswers + " rätt (" + numberOfCorrectAnswers + "/8)");
+
+        System.out.println("Nu kommer nästa del!");
+        System.out.println();
+
+    }
+
+    /*static void looptest() {
         // Create an array
         Integer[] array = new Integer[]{1, 2, 3, 4};
 
-        Integer.add("ok")
+        Integer.add("ok");
         //int[] array = new int[]{1, 2, 3, 4}; //does not work
 
         // Shuffle the elements in the array
@@ -28,7 +101,7 @@ public class Uppgifter {
         Collections.shuffle(l);
 
         System.out.println(l);
-    }
+    }*/
 
     static void byteValueTest() {
         Scanner input = new Scanner( System.in );
