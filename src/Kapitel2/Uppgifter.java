0d3667b2ec872b667a55d0bc12c4aed28e533607
Minoo7 <accounts@minoo.pro>
2018-12-17 14:54:12 +0100

    2018_12_17

--- src/Kapitel2/Uppgifter.java
+++ src/Kapitel2/Uppgifter.java
@@ -1,10 +1,13 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 import java.util.*;
 
 public class Uppgifter {
 
     public static void main(String[] args) {
-        looptest();
+        lektest();
+        //repetitionsfrågor();
+        //looptest();
         //byteValueTest();
         //System.out.println();
         //uppgift2_1();
@@ -14,11 +17,195 @@ public class Uppgifter {
         //v50Torsdag();
     }
 
-    static void looptest() {
+    static void lektest() {
+        double skoNr = 3.9;
+        int helaSkoNr = (int)skoNr;
+        System.out.println(helaSkoNr);
+    }
+
+    static void repetitionsfrågor() {
+        Scanner input = new Scanner( System.in );
+        int numberOfCorrectAnswers = 0;
+
+        System.out.println("Skriv de commands som passar varje uppgift");
+        System.out.println("Använd tal och tecken som variabel namn beroende på uppgiften");
+        System.out.println();
+        System.out.println("a)  Reservera 4 byte för ett nytt heltal.");
+        String a = input.nextLine();
+        if (a.equals("int tal;")) {
+            System.out.println("Rätt!");
+            numberOfCorrectAnswers = numberOfCorrectAnswers + 1;
+        }
+        else {
+            System.out.println("Fel!");
+        }
+        System.out.println();
+
+        System.out.println("b)  Reservera 2 byte för ett nytt heltal och ge det värdet 5");
+        String b = input.nextLine();
+        if (b.equals("short tal = 5;")) {
+            System.out.println("Rätt!");
+            numberOfCorrectAnswers = numberOfCorrectAnswers + 1;
+        }
+        else {
+            System.out.println("Fel!");
+        }
+        System.out.println();
+
+        System.out.println("c)  Reservera 4 byte för ett nytt flyttal och ge värdet 3,3.");
+        String c = input.nextLine();
+        if (c.equals("float tal = 3.3f;")) {
+            System.out.println("Rätt!");
+            numberOfCorrectAnswers = numberOfCorrectAnswers + 1;
+        }
+        else {
+            System.out.println("Fel!");
+        }
+        System.out.println();
+
+        System.out.println("d)  Reservera 8 byte för ett nytt flyttal och ge värdet 9,90.");
+        String d = input.nextLine();
+        if (d.equals("double tal = 9.90;")) {
+            System.out.println("Rätt!");
+            numberOfCorrectAnswers = numberOfCorrectAnswers + 1;
+        }
+        else {
+            System.out.println("Fel!");
+        }
+        System.out.println();
+
+        System.out.println("e)  Reservera 2 byte för ett nytt tecken och ge det värdet T.");
+        String e = input.nextLine();
+        if (e.equals("char tecken = T;")) {
+            System.out.println("Rätt!");
+            numberOfCorrectAnswers = numberOfCorrectAnswers + 1;
+        }
+        else {
+            System.out.println("Fel!");
+        }
+        System.out.println();
+
+        System.out.println();
+        System.out.println("Bra jobbat!");
+        System.out.println("Du fick i denna del " + numberOfCorrectAnswers + " rätt (" + numberOfCorrectAnswers + "/5)");
+
+        System.out.println("Nu kommer nästa del!");
+        System.out.println();
+
+        int numberOfCorrectAnswers2 = 0;
+        System.out.println("Svara på följande frågor");
+        System.out.println();
+        System.out.println("Vilket byte utrymme har?");
+        System.out.println();
+
+        System.out.print("Short = " );
+        float answerShort = input.nextFloat();
+        if (answerShort == 2) {
+            System.out.println("Rätt");
+            numberOfCorrectAnswers2 = numberOfCorrectAnswers2 + 1;
+        }
+        else {
+            System.out.println("Fel!");
+            System.out.println("Rätt svar är 2 byte");
+        }
+        System.out.println();
+
+        System.out.print("int = ");
+        float answerInt = input.nextFloat();
+        if (answerInt == 4) {
+            System.out.println("Rätt");
+            numberOfCorrectAnswers2 = numberOfCorrectAnswers2 + 1;
+        }
+        else {
+            System.out.println("Fel!");
+            System.out.println("Rätt svar är 4 byte");
+        }
+        System.out.println();
+
+        System.out.print("long = ");
+        float answerLong = input.nextFloat();
+        if (answerLong == 8) {
+            System.out.println("Rätt");
+            numberOfCorrectAnswers2 = numberOfCorrectAnswers2 + 1;
+        }
+        else {
+            System.out.println("Fel!");
+            System.out.println("Rätt svar är 8 byte");
+        }
+        System.out.println();
+
+        System.out.print("float = ");
+        float answerFloat = input.nextFloat();
+        if (answerFloat == 4) {
+            System.out.println("Rätt");
+            numberOfCorrectAnswers2 = numberOfCorrectAnswers2 + 1;
+        }
+        else {
+            System.out.println("Fel!");
+            System.out.println("Rätt svar är 4 byte");
+        }
+        System.out.println();
+
+        System.out.print("double = ");
+        float answerDouble = input.nextFloat();
+        if (answerLong == 8) {
+            System.out.println("Rätt");
+            numberOfCorrectAnswers2 = numberOfCorrectAnswers2 + 1;
+        }
+        else {
+            System.out.println("Fel!");
+            System.out.println("Rätt svar är 8 byte");
+        }
+        System.out.println();
+
+        System.out.print("char = ");
+        float answerChar = input.nextFloat();
+        if (answerChar == 2) {
+            System.out.println("Rätt");
+            numberOfCorrectAnswers2 = numberOfCorrectAnswers2 + 1;
+        }
+        else {
+            System.out.println("Fel!");
+            System.out.println("Rätt svar är 2 byte");
+        }
+        System.out.println();
+
+        System.out.print("boolean = ");
+        float answerBoolean = input.nextFloat();
+        if (answerBoolean == 1) {
+            System.out.println("Rätt");
+            numberOfCorrectAnswers2 = numberOfCorrectAnswers2 + 1;
+        }
+        else {
+            System.out.println("Fel!");
+            System.out.println("Rätt svar är 1 byte");
+        }
+        System.out.println();
+
+        System.out.print("String = ");
+        float answerString = input.nextFloat();
+        if (answerString == 2) {
+            System.out.println("Rätt");
+            numberOfCorrectAnswers2 = numberOfCorrectAnswers2 + 1;
+        }
+        else {
+            System.out.println("Fel!");
+            System.out.println("Rätt svar är 2 byte");
+        }
+
+        System.out.println();
+        System.out.println("Bra jobbat!");
+        System.out.println("Du fick i denna del" + numberOfCorrectAnswers2 + " rätt (" + numberOfCorrectAnswers + "/8)");
+        System.out.println();
+        int totalcorrect = numberOfCorrectAnswers + numberOfCorrectAnswers2;
+        System.out.println("Du fick totalt" + totalcorrect + "rätt (" + totalcorrect + "/13");
+    }
+
+    /*static void looptest() {
         // Create an array
         Integer[] array = new Integer[]{1, 2, 3, 4};
 
-        Integer.add("ok")
+        Integer.add("ok");
         //int[] array = new int[]{1, 2, 3, 4}; //does not work
 
         // Shuffle the elements in the array
@@ -28,7 +215,7 @@ public class Uppgifter {
         Collections.shuffle(l);
 
         System.out.println(l);
-    }
+    }*/
 
     static void byteValueTest() {
         Scanner input = new Scanner( System.in );
