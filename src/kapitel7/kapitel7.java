14348a1d5e3c478df6ff13d574a90208bc3a6fae
ezk658 <accounts@minoo.pro>
2019-01-25 11:28:42 +0100

    2019_01_25

--- src/kapitel7/kapitel7.java
+++ src/kapitel7/kapitel7.java
@@ -15,7 +15,7 @@ public class kapitel7 {
         Scanner input = new Scanner (System.in);
 
         //Övning 7.2
-        int tanken = 50;
+        /*int tanken = 50;
         double pris = 9.50;
         System.out.println("Hur mycket etanol finns det i tanken?");
         double etanol = input.nextDouble();
@@ -28,6 +28,46 @@ public class kapitel7 {
             System.out.println("Du kan fylla på " + etanol);
             System.out.println("Att fylla på tanken fullt skulle kostar " + (etanol*pris) + " kr");
 
+        }*/
+
+        //Övning 7.3
+        /*System.out.println("Vad är din ålder?");
+        int ålder = input.nextInt();
+        if (15 <= ålder && ålder <= 65) {
+            System.out.println("Din bussbiljett kostar 10 kr");
+        }
+        else {
+            System.out.println("Din bussbiljett kostar 5 kr");
+        }*/
+
+        //Övning 7.6
+        /*double prisPerSkiva = 9.9;
+        System.out.println("Hur många skivor ska du köpa?");
+        double antalSkivor = input.nextInt();
+        double pris = prisPerSkiva * antalSkivor;
+        if (antalSkivor >= 10) {
+            pris*=0.95;
+        }
+        if (antalSkivor >= 50) {
+            pris*=0.9;
+        }
+        if (antalSkivor >= 100) {
+            pris*= 0.85;
+        }
+        System.out.println("Din totala kostnad är " + pris + " kr");*/
+
+        //Övning 7.7
+        System.out.println("MENY");
+        System.out.println("1.  Insättning");
+        System.out.println("2.  Uttag");
+        System.out.println("3.  Visa behållning");
+        double behållning = 100;
+        int val = input.nextInt();
+        switch(val) {
+            case 1:
+                System.out.println("Hur mycket vill du insätta?");
+                System.out.print("Insättning: ");
+                double insättning = input.nextDouble();
         }
     }
 
