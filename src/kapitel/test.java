4064f84587eb694e808fd166b5b2b31edc762af5
minoo7 <accounts@minoo.pro>
2019-04-01 14:53:45 +0200

    2019_04_01

--- src/kapitel/test.java
+++ src/kapitel/test.java
@@ -10,15 +10,29 @@ public class test {
     static final Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        //test3dcube();
-        String output = String.format("%s = %d", "joe", 35);
-        System.out.printf(output);
-        System.out.printf("%d", 93);
-        //System.out.printf("Hello %!", "World");
+        test3dcube(5);
     }
 
-    static void test3dcube() {
-        System.out.println("*************");
+    /*static int test3dcube(int a) {
+        /*System.out.print(
+                "  " + cubeline(a, "-") +
+                "\n /" + forIX(a, " ") + "/|\n" +
+                cubeline(a, "-") + " |\n" +
+                "|" + forIX(a, " ") + "| +\n" +
+                "|" + forIX(a, " ") + "|/\n" +
+                cubeline(a, "-")
+                );
+        /*cubeline(a, "-");
+        System.out.print("\n /");
+        for (int i = 0; i < a; i++) {
+            System.out.print(" ");
+        }
+        System.out.print("/|\n");
+        cubeline(a, "-");*/
+
+
+
+        /*System.out.println("*************");
         System.out.println("*\t\t\t*");
         System.out.println("*\t\t\t*");
         System.out.println("*\t  ************");
@@ -29,6 +43,60 @@ public class test {
         System.out.println("\t  ************");
         /*for (int i = 0; i < 25; i++) {
             System.out.println();
-        }*/
+        }
+        return 0;
+    }*/
+    static int test3dcube(int a) {
+        System.out.print(
+                spaceIn(a) + cubeline(a, "-") +
+                "\n /" + forIX(a, " ") + "/|\n" +
+                cubeline(a, "-") + " |\n" +
+                "|" + forIX(a, " ") + "| +\n" +
+                "|" + forIX(a, " ") + "|/\n" +
+                cubeline(a, "-")
+                );
+        /*cubeline(a, "-");
+        System.out.print("\n /");
+        for (int i = 0; i < a; i++) {
+            System.out.print(" ");
+        }
+        System.out.print("/|\n");
+        cubeline(a, "-");*/
+
+        return 0;
+    }
+    static String cubeline(int a, String b) {
+        String line = "+" + forIX(a, b) + "+";
+        return line;
+    }
+    static String spaceIn(int a) {
+        int s = (int) a/3;
+        String t = "";
+        for (int i = 0; i < s; i++) {
+            t += " ";
+        }
+        return t;
+    }
+    static String cubelineN(int a) {
+        String l = "";
+        for (int i = 0; i < a; i++) {
+            l += "-";
+        }
+        return l;
+    }
+    static String cubespace(int a) {
+        String s = "";
+        for (int i = 0; i < a; i++) {
+
+        }
+        return "";
+    }
+    //static String forIX(int a, String b, String bAdd) {
+    static String forIX(int a, String b) {
+        String c = "";
+        for (int i = 0; i < a; i++) {
+            c += b;
+        }
+        return c;
     }
 }
