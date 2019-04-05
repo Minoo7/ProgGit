9e0101335adab1e9432d481220f2fbf1354163e2
minoo7 <accounts@minoo.pro>
2019-04-05 11:38:21 +0200

    2019_04_05

---
+++ src/kapitel/EfterKap9Prov/Samlingar.java
@@ -0,0 +1,13 @@
+package kapitel.EfterKap9Prov;
+import java.util.ArrayList;
+
+public class Samlingar {
+
+    public static void main(String[] args) {
+        int[] lista = new int[4];
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("Ok");
+        arrayList.add("Ok");
+        System.out.println(arrayList);
+    }
+}
--- src/kapitel/Tärning.java
+++ src/kapitel/Tärning.java
@@ -4,26 +4,69 @@ import java.sql.SQLOutput;
 import java.util.List;
 import java.util.Scanner;
 import java.util.Arrays;
+import java.util.Random;
 
 
 public class Tärning{
 
+    public static int roll() {
+
+        /*int numberShowing = 0;
+        long[] nummer = new long[6];
+        Random random = new Random();
+        int antalFörsök = 1000;
+        for (int i = 0; i < antalFörsök; i++) {
+                //numberShowing = 0;
+            int numberofsixes = 0;
+            for (int j = 0; j < 6; i++) {
+
+                if(random.nextInt(6) + 1 == 6){
+                    numberofsixes++;
+                }
+            }
+            if(numberofsixes == 3){
+                numberShowing++;
+            }
+        }
+        double p = (numberShowing / antalFörsök) * 100;
+
+
+        return numberShowing;*/
+
+        int numberShowing = 0;
+        int[] nummer = new int[6];
+        Random random = new Random();
+        for (int i = 0; i < 100; i++) {
+            numberShowing = random.nextInt(6) + 1;
+            nummer[numberShowing-1]++;
+        }
+        for (int i = 0; i < 6; i++) {
+            System.out.println(i + ": " + nummer[i]);
+        }
+        double nummercheck = (double) nummer[5];
+        System.out.println((nummer[5]/6) / 100d);
+        //return numberShowing;
+        return 1;
+    }
+
     public static void main(String[] args) {
-        System.out.println(kasta());
+        //kasta();
+
+        roll();
+
+        /*Random random = new Random();
+        for (int i = 0; i < 6; i++) {
+            System.out.println(random.nextInt(6) + 1);
+        }*/
+        //System.out.println(kasta());
     }
 
     private static int sidor = 6;
 
-    /*public static Tärning(){
-        sidor=6;
-    }
-
-    public Tärning(int sidor){
-        this.sidor=sidor;
-    }*/
-
     public static int kasta(){
-        return (int)(Math.random()*sidor+1);
+
+        //return (int)(Math.random()*sidor+1);
+        return 5;
     }
 
 }
