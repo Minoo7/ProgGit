0a75e57cfd55dc4cd311e5c09e16203ad431a806
minoo7 <accounts@minoo.pro>
2019-04-29 14:56:37 +0200

    2019_04_29

--- src/UppgifterMetoder/UppgifterProjekt.java
+++ src/UppgifterMetoder/UppgifterProjekt.java
@@ -12,6 +12,15 @@ public class UppgifterProjekt {
         TreIRad();
     }
     public static void TreIRad() {
+        char _0_0 = ' ';
+        char _0_1 = ' ';
+        char _0_2 = ' ';
+        char _1_0 = '_';
+        char _1_1 = '_';
+        char _1_2 = '_';
+        char _2_0 = '_';
+        char _2_1 = '_';
+        char _2_2 = '_';
         System.out.println("Vill du spela mot datorn [1] eller mot annan spelare [2]?");
         int val = input.nextInt();
         switch (val) {
@@ -21,53 +30,52 @@ public class UppgifterProjekt {
             case (2):
                 System.out.println("Du spelar mot en annan användare");
                 System.out.println("Ange koordinaterna till din markering,  Ex: 2,1 = 2 upp från 0, 1 till höger av 0");
-                //System.out.println("_ _ _\n_ _ _\n_ _ _\n");
-                System.out.println(
-                        "___|___|___\n" +
-                        "___|___|___\n" +
-                        "   |   |\n");
-                //ANVÄND SWITCH CASE ISTÄLLET FÖR IF DET ÄR DET VALET,     static void arrayListExempel() {
-                //        System.out.println("ARRAYLIST EXEMPEL");
-                //        // En arrayList är en dynamisk lista som kan ändra storlek.
-                //        // Du kan enkelt lägga till och ta bort element från en arrayList med add(element) och remove(index)
-                //        // Du kan även infoga ett element på ett visst index med add(index, element)
-                //        ArrayList<Integer> arrayList = new ArrayList<>();
-                //        arrayList.add(5);
-                //        arrayList.add(7);
-                //        arrayList.add(9);
-                //        System.out.println(arrayList);
-                //        arrayList.remove(0);
-                //        System.out.println(arrayList);
-                //        arrayList.add(11);
-                //        System.out.println(arrayList);
-                //        arrayList.add(1,100);
-                //        System.out.println(arrayList);
-                //
-                //        for(int i = 0; i < arrayList.size(); i++){
-                //            System.out.println(arrayList.get(i));
-                //        }
-                //        // Annan typ av syntax för att loopa över en samling.
-                //        for(int i : arrayList) {
-                //            System.out.println(i);
-                //        }
-                // ANVÄND DENNA SLAGS LOOP!
-                char _0_0 = ' ';
-                char _0_1 = ' ';
-                char _0_2 = ' ';
-                char _1_0 = '_';
-                char _1_1 = '_';
-                char _1_2 = '_';
-                char _2_0 = '_';
-                char _2_1 = '_';
-                char _2_2 = '_';
-                System.out.println("Spelare 1: ");
-                String P1_1 = input.next();
-                System.out.println(
-                        "_"+_2_0+"_|_"+_2_1+"_|_"+_2_2+"_\n" +
-                        "_"+_1_0+"_|_"+_1_1+"_|_"+_1_2+"_\n" +
-                        " "+_0_0+" | "+_0_1+" | "+_0_2+" \n");
-                System.out.println();
-                break;
+                String plan = ("_" + _2_0 + "_|_" + _2_1 + "_|_" + _2_2 + "_\n" +
+                               "_" + _1_0 + "_|_" + _1_1 + "_|_" + _1_2 + "_\n" +
+                               " " + _0_0 + " | " + _0_1 + " | " + _0_2 + " \n");
+                boolean goOn = true;
+                boolean win = false;
+                while (win == false) {
+                    goOn = true;
+                    System.out.println(plan);
+                    String P1 = input.next();
+                    switch (P1) {
+                        case("0,0"): if(_0_0 == ' '){_0_0 = 'X';} else{System.out.println("Det finns redan en pjäs där");goOn = false;} break;
+                        case("0,1"): if(_0_1 == ' '){_0_1 = 'X';} else{System.out.println("Det finns redan en pjäs där");goOn = false;} break;
+                        case("0,2"): if(_0_2 == ' '){_0_2 = 'X';} else{System.out.println("Det finns redan en pjäs där");goOn = false;} break;
+                        case("1,0"): if(_1_0 == '_'){_1_0 = 'X';} else{System.out.println("Det finns redan en pjäs där");goOn = false;} break;
+                        case("1,1"): if(_1_1 == '_'){_1_1 = 'X';} else{System.out.println("Det finns redan en pjäs där");goOn = false;} break;
+                        case("1,2"): if(_1_2 == '_'){_1_2 = 'X';} else{System.out.println("Det finns redan en pjäs där");goOn = false;} break;
+                        case("2,0"): if(_2_0 == '_'){_2_0 = 'X';} else{System.out.println("Det finns redan en pjäs där");goOn = false;} break;
+                        case("2,1"): if(_2_1 == '_'){_2_1 = 'X';} else{System.out.println("Det finns redan en pjäs där");goOn = false;} break;
+                        case("2,2"): if(_2_2 == '_'){_2_2 = 'X';} else{System.out.println("Det finns redan en pjäs där");goOn = false;} break;
+                    }
+                    if (goOn == true) {
+                        plan = ("_" + _2_0 + "_|_" + _2_1 + "_|_" + _2_2 + "_\n" + "_" + _1_0 + "_|_" + _1_1 + "_|_" + _1_2 + "_\n" + " " + _0_0 + " | " + _0_1 + " | " + _0_2 + " \n");
+                        System.out.println(plan);
+                        if (/*Horisontella rader*/ (_0_0 == 'X' && _0_1 == 'X' && _0_2 == 'X')  || (_1_0 == 'X' && _1_1 == 'X' && _1_2 == 'X') || (_2_0 == 'X' && _2_1 == 'X' && _2_2 == 'X') || /*Vertikala rader*/ (_0_0 == 'X' && _1_0 == 'X' && _2_0 == 'X') || (_0_1 == 'X' && _1_1 == 'X' && _2_1 == 'X') || (_0_2 == 'X' && _1_2 == 'X' && _2_2 == 'X') || /*Diagonala*/ (_0_0 == 'X' && _1_1 == 'X' && _2_2 == 'X') || (_0_2 == 'X' && _1_1 == 'X' && _2_0 == 'X')) {
+                            win = true;
+                        }
+                        String P2 = input.next();
+                        switch (P2) {
+                            case("0,0"): _0_0 = 'O'; break;
+                            case("0,1"): _0_1 = 'O'; break;
+                            case("0,2"): _0_2 = 'O'; break;
+                            case("1,0"): _1_0 = 'O'; break;
+                            case("1,1"): _1_1 = 'O'; break;
+                            case("1,2"): _1_2 = 'O'; break;
+                            case("2,0"): _2_0 = 'O'; break;
+                            case("2,1"): _2_1 = 'O'; break;
+                            case("2,2"): _2_2 = 'O'; break;
+                        }
+                        if (/*Horisontella rader*/ (_0_0 == 'O' && _0_1 == 'O' && _0_2 == 'O')  || (_1_0 == 'O' && _1_1 == 'O' && _1_2 == 'O') || (_2_0 == 'O' && _2_1 == 'O' && _2_2 == 'O') || /*Vertikala rader*/ (_0_0 == 'O' && _1_0 == 'O' && _2_0 == 'O') || (_0_1 == 'O' && _1_1 == 'O' && _2_1 == 'O') || (_0_2 == 'O' && _1_2 == 'O' && _2_2 == 'O') || /*Diagonala*/ (_0_0 == 'O' && _1_1 == 'O' && _2_2 == 'O') || (_0_2 == 'O' && _1_1 == 'O' && _2_0 == 'O')) {
+                            win = true;
+                        }
+                        plan = ("_" + _2_0 + "_|_" + _2_1 + "_|_" + _2_2 + "_\n" + "_" + _1_0 + "_|_" + _1_1 + "_|_" + _1_2 + "_\n" + " " + _0_0 + " | " + _0_1 + " | " + _0_2 + " \n");
+                        System.out.println(plan);
+                    }
+                }
+            break;
         }
     }
 }
